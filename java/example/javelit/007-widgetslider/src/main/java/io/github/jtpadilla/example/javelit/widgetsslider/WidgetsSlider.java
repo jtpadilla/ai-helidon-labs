03063c5da6592c56e1e067819923ae3f4f10bcd5
padilla <padilla@uji.es>
2026-02-26 17:31:00 +0100

    Ejamplos javelit

---
+++ java/example/javelit/007-widgetslider/src/main/java/io/github/jtpadilla/example/javelit/widgetsslider/WidgetsSlider.java
@@ -0,0 +1,24 @@
+package io.github.jtpadilla.example.javelit.widgetsslider;
+
+import io.javelit.core.Jt;
+import io.javelit.core.Server;
+import org.icepear.echarts.Gauge;
+import org.icepear.echarts.charts.gauge.GaugeDataItem;
+import org.icepear.echarts.charts.gauge.GaugeProgress;
+import org.icepear.echarts.charts.gauge.GaugeSeries;
+
+class WidgetsSlider {
+
+    static void main(String[] args) {
+        Server.builder(WidgetsSlider::new, 8080)
+                .build()
+                .start();
+
+    }
+
+    public WidgetsSlider() {
+        var x = Jt.slider("My slider").use();  // 👈 this is a widget
+        Jt.text(x + " squared is " + x * x).use();
+    }
+
+}
