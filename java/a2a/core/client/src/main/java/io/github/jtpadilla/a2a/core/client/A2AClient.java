cd090c910de40f270cae99528b345d53485a2c4b
padilla <padilla@speedycontrol.com>
2026-04-05 11:57:02 +0200

    Implementado a2a

---
+++ java/a2a/core/client/src/main/java/io/github/jtpadilla/a2a/core/client/A2AClient.java
@@ -0,0 +1,4 @@
+package io.github.jtpadilla.a2a.core.client;
+
+public class A2AClient {
+}
---
+++ java/a2a/core/common/src/main/java/io/github/jtpadilla/a2a/core/common/A2AConstants.java
@@ -0,0 +1,9 @@
+package io.github.jtpadilla.a2a.core.common;
+
+public class A2AConstants {
+
+    private A2AConstants() {
+    }
+
+        
+}
