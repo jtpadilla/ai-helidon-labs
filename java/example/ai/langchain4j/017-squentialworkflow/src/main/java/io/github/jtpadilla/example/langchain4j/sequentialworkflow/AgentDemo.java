e97c7f407e7a16d2ed67360411140ae09cdc1667
padilla <padilla@speedycontrol.com>
2026-04-14 19:03:17 +0200

    langchain4j examples.

--- java/example/ai/langchain4j/017-squentialworkflow/src/main/java/io/github/jtpadilla/example/langchain4j/sequentialworkflow/AgentDemo.java
+++ java/example/ai/langchain4j/017-squentialworkflow/src/main/java/io/github/jtpadilla/example/langchain4j/sequentialworkflow/AgentDemo.java
@@ -1,11 +1,10 @@
 package io.github.jtpadilla.example.langchain4j.sequentialworkflow;
 
-import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.agentic.Agent;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
-import dev.langchain4j.service.AiServices;
-import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import io.helidon.config.Config;
 
 public class AgentDemo {
@@ -23,26 +22,19 @@ public class AgentDemo {
                 .logRequestsAndResponses(true)
                 .build();
 
-        interface Assistant  {
-            String chat(@MemoryId int memoryId, @UserMessage String message);
+        interface CreativeWriter {
+
+            @UserMessage("""
+            You are a creative writer.
+            Generate a draft of a story no more than
+            3 sentences long around the given topic.
+            Return only the story and nothing else.
+            The topic is {{topic}}.
+            """)
+            @Agent("Generates a story based on the given topic")
+            String generateStory(@V("topic") String topic);
         }
 
-        Assistant assistant = AiServices.builder(Assistant.class)
-                .chatModel(model)
-                .chatMemoryProvider(memoryId -> MessageWindowChatMemory.withMaxMessages(10))
-                .build();
-
-        String answerToKlaus = assistant.chat(1,"Hola, mi nombre es Klaus");
-        System.out.println("1) " + answerToKlaus); // Hello, how can I help you?
-
-        String answerToFrancine = assistant.chat(2, "Hola, mi nombre es Francine");
-        System.out.println("2) " + answerToFrancine); // Hello, how can I help you?
-
-        answerToKlaus = assistant.chat(1,"Cual es mi nombre?");
-        System.out.println("1) " + answerToKlaus); // Hello, how can I help you?
-
-        answerToFrancine = assistant.chat(2, "Cual es mi nombre?");
-        System.out.println("2) " + answerToFrancine); // Hello, how can I help you?
 
     }
 
