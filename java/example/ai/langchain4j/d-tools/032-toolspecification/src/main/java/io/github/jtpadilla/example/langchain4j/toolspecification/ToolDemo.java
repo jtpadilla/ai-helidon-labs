d7598a60b3b7c443b168cd11d592428dc16b8994
padilla <padilla@speedycontrol.com>
2026-04-20 09:11:34 +0200

    langchain4j examples.

--- java/example/ai/langchain4j/d-tools/032-toolspecification/src/main/java/io/github/jtpadilla/example/langchain4j/toolspecification/ToolDemo.java
+++ java/example/ai/langchain4j/d-tools/032-toolspecification/src/main/java/io/github/jtpadilla/example/langchain4j/toolspecification/ToolDemo.java
@@ -1,9 +1,13 @@
 package io.github.jtpadilla.example.langchain4j.toolspecification;
 
+import dev.langchain4j.agent.tool.ToolSpecification;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.request.json.JsonObjectSchema;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 import io.github.jtpadilla.example.util.GoogleModels;
 import io.helidon.config.Config;
+
+import java.util.List;
 
 public class ToolDemo {
 
@@ -11,6 +15,16 @@ public class ToolDemo {
             () -> new IllegalStateException("La clave de configuración 'gemini-api-key' es obligatoria"));
 
     public static void main(String[] args) {
+
+        ToolSpecification toolSpecification = ToolSpecification.builder()
+                .name("getWeather")
+                .description("Returns the weather forecast for a given city")
+                .parameters(JsonObjectSchema.builder()
+                        .addStringProperty("city", "The city for which the weather forecast should be returned")
+                        .addEnumProperty("temperatureUnit", List.of("CELSIUS", "FAHRENHEIT"))
+                        .required("city") // the required properties should be specified explicitly
+                        .build())
+                .build();
 
         ChatModel chatModel = GoogleAiGeminiChatModel.builder()
                 .apiKey(API_KEY)
